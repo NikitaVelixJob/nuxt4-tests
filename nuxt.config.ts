9e22d62873c88922475be0c10a7a67d45375d0a5
NikitaVelixJob <NikitaVelixJob@yahoo.com>
2024-05-10 00:48:59 +0500

    feat: added example to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
+  // To re-enable _all_ Nuxt v3 behavior, set the following options:
+  // srcDir: '.',
+  // dir: {
+  //   app: 'app'
+  // },
+  // experimental: {
+  //   compileTemplate: true,
+  //   templateUtils: true,
+  //   relativeWatchPaths: true,
+  //   defaults: {
+  //     useAsyncData: {
+  //       deep: true
+  //     }
+  //   }
+  // },
+  // unhead: {
+  //   renderSSRHeadOptions: {
+  //     omitLineBreaks: false
+  //   }
+  // }
 })
